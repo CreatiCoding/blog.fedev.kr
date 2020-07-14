9c17528a3f66b39be7423d54a9f8ced82f071c13
CreatiCoding <creaticoding@gmail.com>
2020-07-15 04:13:40 +0900

    feat: 전체 구조 및 footer 수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <router-view class="d-b main-header" name="MainHeader"></router-view>
+    <MarginTag top="62px" />
     <router-view class="d-b main-container"></router-view>
+    <MarginTag top="5rem" />
     <router-view class="d-b main-footer" name="MainFooter"></router-view>
     <div class="modal" v-if="Object.values(modal_list).filter(e=>e).length" @click="$modal.hideAll">
       <AlertModal v-if="modal_list.alert" :state="modal_list.alert" />
@@ -13,7 +15,8 @@
 export default {
   name: "App",
   components: {
-    AlertModal: () => import("./modals/AlertModal")
+    AlertModal: () => import("./modals/AlertModal"),
+    MarginTag: () => import("./presentation/atoms/MarginTag")
   },
   data() {
     return {
@@ -29,13 +32,24 @@ export default {
 .stackedit-container {
   margin-top: 62px;
 }
+img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
 <style lang="scss" scoped>
 .main-header {
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  width: 100%;
   height: 62px;
+  background: white;
 }
 .main-container {
   min-height: calc(100vh - 100px);
+  max-width: 1024px;
+  margin: 0 auto;
 }
 .modal {
   position: fixed;
