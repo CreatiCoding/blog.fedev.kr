c2a6510396ad5588f466333e509eec17551b32ed
CreatiCoding <creaticoding@gmail.com>
2020-07-16 01:50:00 +0900

    feat: 리스트 페이지 아이템 UI 수정

--- src/pages/DocumentListPage.vue
+++ src/pages/DocumentListPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="document-list-page">
-    <MarginTag top="50px" />
     <div>
       <h1>게시글</h1>
     </div>
@@ -9,6 +8,7 @@
       :key="`document_list-${index}-${document.document_id}`"
     >
       <ButtonTag
+        class="document-item"
         @mouseenter.native="
           (event) => (event.target.style.background = 'skyblue')
         "
@@ -17,9 +17,10 @@
         "
         :click="() => $router.push(`/document/detail/${document.document_id}`)"
       >
-        <div>{{ document.document_id }}</div>
-        <div>{{ document.title }}</div>
-        <div>{{ document.created_at }}</div>
+        <!-- <div>{{ document.document_id }}</div> -->
+        <h2 class="document-title">{{ document.title }}</h2>
+        <div class="t-r">{{ document.created_at }}</div>
+        <span class="document-contents">{{ document.contents.slice(0,100) }}</span>
       </ButtonTag>
     </div>
   </div>
@@ -28,7 +29,6 @@
 export default {
   name: "DocumentListPage",
   components: {
-    MarginTag: () => import("../presentation/atoms/MarginTag"),
     ButtonTag: () => import("../presentation/atoms/ButtonTag")
   },
   data() {
@@ -50,5 +50,42 @@ export default {
 .document-list-page {
   // background: salmon;
   margin: 0 auto;
+  .document-item {
+    padding: 1rem;
+    margin: 2rem 0;
+    box-shadow: 2px 2px 8px #c8c8c8;
+    .document-title {
+      margin: 10px 0;
+      display: inline-block;
+      width: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: normal;
+      line-height: 120%;
+      height: 120%;
+      text-align: left;
+      word-wrap: break-word;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+    }
+    .document-contents {
+      margin: 10px 0;
+      display: inline-block;
+      width: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: normal;
+      line-height: 120%;
+      height: 240%;
+      text-align: left;
+      word-wrap: break-word;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+  }
 }
 </style>
