86d0461be8330d28b082f6243327973700ff217d
CreatiCoding <creaticoding@gmail.com>
2020-07-16 01:48:39 +0900

    feat: 패딩 컴포넌트 추가

---
+++ src/presentation/atoms/PaddingTag.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :style="{
+      paddingTop: top,
+      paddingRight: right,
+      paddingBottom: bottom,
+      paddingLeft: left,
+      width: width,
+      height: height,
+    }"
+  ></div>
+</template>
+<script>
+export default {
+  name: "PaddingTag",
+  props: {
+    top: { type: String, default: "auto" },
+    right: { type: String, default: "auto" },
+    bottom: { type: String, default: "auto" },
+    left: { type: String, default: "auto" },
+
+    width: { type: String, default: "auto" },
+    height: { type: String, default: "auto" }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
